5403c788267efdffb6890cf69b3e9d1e5e1f6161
Kevin <actuskevin@gmail.com>
2024-07-20 11:13:43 +0200

    Ajout de Router + Index.html

---
+++ Router/allRoutes.js
@@ -0,0 +1,16 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/visit", "Habitats & animaux", "/pages/visit.html"),
+    new Route("/service", "Services", "/pages/service.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    //new Route("/editPassword", "Changement de mot de passe", "/pages/authentifiation/editPassword.html"),
+    //new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+    //new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Zoo Arcadia";
