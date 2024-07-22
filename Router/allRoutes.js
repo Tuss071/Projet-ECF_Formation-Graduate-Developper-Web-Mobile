82cd90baa16610545fdf556b5b314ebcf3a84085
Kevin <actuskevin@gmail.com>
2024-07-22 23:38:10 +0200

    modif Allroutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/visit", "Habitats & animaux", "/pages/visit.html"),
+    new Route("/visit", "Habitats & animaux", "./pages/visit.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/signin", "Connexion", "/pages/authentification/signin.html"),
