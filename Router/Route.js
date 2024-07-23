0048a3fc3ec015a74d79f0a8bd3c57c5ad86a31b
Kevin <actuskevin@gmail.com>
2024-07-23 17:09:58 +0200

    modif index

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,8 @@
-export default class Route {
+export default class Routes {
     constructor(url, title, pathHtml, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
     }
-}
+  }
