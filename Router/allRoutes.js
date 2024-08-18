43e1e1ee69e11480f6388d0c55dd5fafe959e956
Kevin <actuskevin@gmail.com>
2024-08-18 17:34:15 +0200

    modif route

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,11 +2,11 @@ import Route from "../Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/visit", "Habitats & animaux", "./pages/visit.html", []),
-    new Route("/services", "Services", "/pages/services.html", []), 
-    new Route("/contact", "Contact", "/pages/contact.html", []),
-    new Route("/signin", "Connexion", "/pages/authentification/signin.html", "/RenduDynamic_js/auth/signin.js", ["admin","employe", "veterinary"]),
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/visit", "Habitats & animaux", "./pages/visit.html"),
+    new Route("/services", "Services", "/pages/services.html"), 
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/signin", "Connexion", "/pages/authentification/signin.html", "/RenduDynamic_js/auth/signin.js"),
     //new Route("/signup","Création de compte utilisateur", "/pages/authentification/signup.html", ["admin"]),
 ];
 
